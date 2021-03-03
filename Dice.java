7cbb914cf6aea3bbd4b8873e94ffef6800a3f8c7
BMann-png <79931740+BMann-png@users.noreply.github.com>
2021-03-03 11:20:09 -0700

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,10 @@
+package myClasses;
+
+import java.util.Random;
+
+public class Dice {
+    public static int roll(int min, int max){
+        Random randy = new Random();
+        return min - randy.nextInt(max - min + 1);
+    }
+}
